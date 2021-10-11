b31f924b538f2bef22195d91eda673ac642e35e2
Mar-bh <89352085+Mar-bh@users.noreply.github.com>
2021-10-11 18:44:28 -0500

    Add files via upload

---
+++ avance_proyecto integradorCopia.py
@@ -0,0 +1,43 @@
+import random
+
+def crearFilaEc(n): #n es entero y su range es entre 2 y 8
+
+    variables = {1:"a",2:"b",3:"c",4:"d",5:"e",6:"f",7:"g",8:"h"}
+    valores = {}  #valor de cada variable
+    coeficiente = [] #lista de coeficientes
+    ecuacion = ""
+    suma = 0
+    
+    for i in range (1,n+1):
+        valor = random.randint(-10,10)
+        valores[variables[i]] = valor
+    
+   # print(valores)
+    for k in range(0,n):
+        ecuacion = ""
+        coeficiente = []
+        suma = 0
+        
+        for i in range (1,n+1):
+            coeficiente.append(random.randint(1,10))
+            espositivo = random.randint(0,1)
+            #print(espositivo)
+            
+            if espositivo == 0:
+                coeficiente[i-1] = coeficiente[i-1]*-1
+                      
+            if coeficiente[i-1] >= 1 and i>1:
+                ecuacion = ecuacion+"+"+(str(coeficiente[i-1])+variables[i])
+            else:
+                ecuacion = ecuacion+(str(coeficiente[i-1])+variables[i])
+            
+            suma += coeficiente[i-1]*valores[variables[i]]
+            #print(suma)
+            
+        print(f"{ecuacion} = {suma}")
+        #print(coeficiente)
+
+
+#crearFilaEc(2)
+#crearFilaEc(3)
+crearFilaEc(4)
