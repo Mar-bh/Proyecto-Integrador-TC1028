5074229028d282897a6c7ca7d0a1492d95e59179
Mariana <marianabhdez@gmail.com>
2021-10-15 11:43:46 -0500

    Avance

--- avance_proyecto integradorCopia.py
+++ avance_proyecto integradorCopia.py
@@ -1,6 +1,7 @@
 import random
 
-def crear_filas_ecuaciones(n,variables,valores): #n es entero y su range es entre 2 y 8
+def crear_filas_ecuaciones(n,variables,valores):  #función que crea y muestra las ecuaciones del sistema
+    #n es entero y su range es entre 2 y 8
     
    # print(valores)
     for k in range(0,n):
@@ -9,44 +10,61 @@ def crear_filas_ecuaciones(n,variables,valores): #n es entero y su range es entr
         suma = 0
         
         for i in range (1,n+1):
-            lista_coeficientes.append(random.randint(1,10))
-            espositivo = random.randint(0,1)
+            lista_coeficientes.append(random.randint(1,10))          #asignamos coeficientes al azar
+            espositivo = random.randint(0,1)                         #asignamos una valor positivo o negativo al azar
             #print(espositivo)
             
-            if espositivo == 0:
+            if espositivo == 0:                                              
                 lista_coeficientes[i-1] = lista_coeficientes[i-1]*-1
                       
-            if lista_coeficientes[i-1] >= 1 and i>1:
+            if lista_coeficientes[i-1] >= 1 and i>1:               
                 ecuacion = ecuacion+"+"+(str(lista_coeficientes[i-1])+variables[i])
             else:
                 ecuacion = ecuacion+(str(lista_coeficientes[i-1])+variables[i])
             
-            suma += lista_coeficientes[i-1]*valores[variables[i]]
+            suma += lista_coeficientes[i-1]*valores[variables[i]]       #se suma el coeficiente multiplicado por el valor de la variable
             #print(suma)
             
         print(f"{ecuacion} = {suma}")
         #print(coeficiente)
 
-def verificar_soluciones(valores,variables):
+def verificar_soluciones(valores,variables):   #función que pide y verifica la solución del usuario
     #print(soluciones)
-    soluciones = {}
+    soluciones = {}                               #diccionario vacío en el que se guardaran las soluciones dadas
     for i in range(1, len(valores) + 1):
-        soluciones[variables[i]] = int(input(f"El valor de la variable {variables[i]}: "))
-    
-    if soluciones == valores:
-        print("Solución correcta") 
+        respuesta = "11" 
+        while type(respuesta) == str:
+            respuesta = input(f"El valor de la variable {variables[i]}: ")
+            if respuesta.isnumeric() ==  False:
+                respuesta = "-11"  
+                print("El valor de la variable debe ser un entero")          
+            else:   
+                respuesta = int(respuesta)                     
+                soluciones[variables[i]] = respuesta      #se pide que el usuario ingrese la solución y se agrega al diccionario
+                                                                                                #la variables es el apendice, la solución el valor asignado a la variable
+    if soluciones == valores:                                                                   #verificamos si el diccionario original con los valores es igual al nuevo creado
+        print("Solución correcta")                                                              #si son iguales, dio la solución correcta
     else:
-        print("Solución incorrecta, los valores correctos son: ") 
+        print("Solución incorrecta, los valores correctos son: ")              #si no son iguales, despliega las soluciones
         for i in range(1, len(valores) + 1):
             print(f"{variables[i]} = {valores[variables[i]]}")
 
-variables = {1:"a",2:"b",3:"c",4:"d",5:"e",6:"f",7:"g",8:"h"}
-valores = {}  #valor de cada variable
-n = 0
-while (n<2 or n>8):
-   n = int(input("Ingrese el número de variables: "))
+variables = {1:"a",2:"b",3:"c",4:"d",5:"e",6:"f",7:"g",8:"h"}  #diccionario de variables
+valores = {}  #diccionario vacío con valor de cada variable  
+n = "0"
+while (int(n)<2 or int(n)>8):
+   n = input("Ingrese el número de variables: ")  #pedimos la entrada n
+   if n.isnumeric() == False:                      #se verifica si el string puesto es númerico
+       n="0"                                       #si no lo es, asigna 0 a n, manda el mensaje y lo vuelve a pedir
+       print("ERROR. La variable a ingresar debe ser un número entero entre 2 y 8")  
+   elif (int(n)<2 or int(n)>8):                                            #n es númerico pero fuera de rango
+       print("ERROR. La variable a ingresar debe ser un número entero entre 2 y 8")
 
-for i in range (1,n+1):
+n = int(n)    #convertimos a entero el valor de n aceptado
+#print(n)
+
+random.seed(10)
+for i in range (1,n+1):                   #se crea el diccionario con los valores de cada variable
     valor = random.randint(-10,10)
     valores[variables[i]] = valor
     
